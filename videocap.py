daaf5f8a2431709c5ba58c619395aa2fd8206402
Jason Stanley <js41074546@gmail.com>
2018-12-14 17:45:53 +0000

    Video Camera

---
+++ videocap.py
@@ -0,0 +1,10 @@
+import cv2,time
+video=cv2.VideoCapture(0)
+while True:
+    check,frame=video.read()
+    cv2.imshow("Capture",frame)
+    key=cv2.waitKey(1)  
+    if key==ord('q'):
+        break
+video.release()
+cv2.destroyAllWindows()
